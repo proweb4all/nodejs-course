1a47e9f6504fb442eb51f4e1517e6dbbfd2be1ce
proweb4all <proweb4all@narod.ru>
2020-02-25 15:35:35 +0300

    2.10 - HTML-modules

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-  res.write('<h1>Hello from NodeJS</h1>')
-  res.write('<p>Description</p>')
-  res.end(`
-    <div style='color: red'>End result</div>
-  `)
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8'
+  })
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      fs.readFile(
+        path.join(__dirname, 'views', 'index.html'), 'utf-8',
+        (err, content) => {
+          if (err) { throw err }
+          res.end(content)
+        }
+      )
+    } else if (req.url === '/about') {
+      fs.readFile(
+        path.join(__dirname, 'views', 'about.html'), 'utf-8',
+        (err, content) => {
+          if (err) { throw err }
+          res.end(content)
+        }
+      )
+    }
+  } else if (req.method === 'POST') {
+    const body = []
+    req.on('data', data => {
+      body.push(Buffer.from(data))
+    })
+    req.on('end', () => {
+      const message = body.toString().split('=')[1]
+      res.end(`
+        <h2>Ваше сообщение: ${message}</h2>
+      `)
+    })
+  }
 })
 
 server.listen(3000, () => {
